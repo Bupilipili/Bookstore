1e8fe5aefe73099f1d1a7ba3899dd5fe9ad86159
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-17 17:07:50 +0300

    Update BookItem.js

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,8 +3,14 @@ import React from 'react';
 function BookItem({ book, onDeleteBook }) {
   return (
     <li>
-      <p>Title: {book.title}</p>
-      <p>Author: {book.author}</p>
+      <p>
+        Title:
+        {book.title}
+      </p>
+      <p>
+        Author:
+        {book.author}
+      </p>
       <button onClick={() => onDeleteBook(book.id)}>Delete</button>
     </li>
   );
