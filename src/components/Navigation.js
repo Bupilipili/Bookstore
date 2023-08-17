b393815def668d5a2637c7adef900a485c33126d
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-17 20:58:26 +0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 function Navigation() {
   return (
     <nav>
-      <Link to="/books">Books</Link>
+      <h1>Bookstore CMS</h1>
+      <Link to="/">Books</Link>
       <Link to="/categories">Categories</Link>
     </nav>
   );
