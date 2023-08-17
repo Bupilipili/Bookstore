af44d2f47f5142e33267280fd988d298cc2e8c52
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-17 20:58:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BooksPage from './components/BooksPage';
-import CategoriesPage from './components/CategoriesPage';
+import BooksPage from './pages/BooksPage';
+import CategoriesPage from './pages/CategoriesPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/books" element={<BooksPage />} />
+        <Route path="/" element={<BooksPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
     </Router>
