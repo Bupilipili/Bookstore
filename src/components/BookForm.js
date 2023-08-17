a761da5f15b4d039440913a2e696277c82817c7a
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-17 16:59:31 +0300

    Create BookForm.js

---
+++ src/components/BookForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './BookForm.css'
+
+function BookForm({ onAddBook }) {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = { title, author, id: Date.now() };
+    onAddBook(newBook);
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+}
+
+export default BookForm;
