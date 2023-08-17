1df532dbedaec9e9b8c7748b002ca3227b52f4f8
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-17 21:08:19 +0300

    Update BookForm.js

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -14,21 +14,24 @@ function BookForm({ onAddBook }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-      />
-      <button type="submit">Add Book</button>
-    </form>
+    <div>
+      <p>ADD NEW BOOK</p>
+      <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      placeholder="Title"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+    />
+    <input
+      type="text"
+      placeholder="Author"
+      value={author}
+      onChange={(e) => setAuthor(e.target.value)}
+    />
+    <button type="submit">Add Book</button>
+  </form>
+  </div>
   );
 }
 
