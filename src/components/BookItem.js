30a3f500591ac222142d1dffa7ffe131ba6efdbe
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-21 18:41:08 +0300

    Update BookItem.js

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice'; // Adjust the import path
 
-const BookItem = ({ book }) => {
+function BookItem({ book }) {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(book.item_id));
+  };
+
   return (
-    <div>
+    <li>
       <p>Title: {book.title}</p>
       <p>Author: {book.author}</p>
-      <p>Category: {book.category}</p>
-    </div>
+      <p>Category: {book.category}</p> {/* Include the category */}
+      <button onClick={handleRemoveBook}>Remove Book</button>
+    </li>
   );
-};
+}
 
 export default BookItem;
