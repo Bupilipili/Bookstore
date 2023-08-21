2e38b39cc0f9789250f614d944d03c6ae52fe6df
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-21 18:40:56 +0300

    Update BooksPage.js

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid'; // Import uuidv4 function
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem'; // Import your BookItem component
-import { addBook, removeBook } from '../redux/books/booksSlice'; // Import your actions
+import AddBookForm from './AddBookForm'; // Import the AddBookForm component
 
 function BooksPage() {
-  const books = useSelector((state) => state.books);
-  const dispatch = useDispatch();
-
-  const handleAddBook = () => {
-    const newBook = {
-      item_id: uuidv4(), // Use uuidv4 to generate a unique ID
-      title: 'New Book Title',
-      author: 'New Book Author',
-      category: 'Fiction',
-    };
-    dispatch(addBook(newBook));
-  };
-
-  const handleRemoveBook = (bookId) => {
-    dispatch(removeBook(bookId));
-  };
+  const books = useSelector(state => state.books);
 
   return (
     <div>
-      {books.map((book) => (
+      {books.map(book => (
         <div key={book.item_id}>
           <BookItem book={book} />
-          <button onClick={() => handleRemoveBook(book.item_id)}>Remove Book</button>
         </div>
       ))}
 
-      <button onClick={handleAddBook}>Add Book</button>
+      <AddBookForm /> {/* Render the AddBookForm component */}
     </div>
   );
 }
