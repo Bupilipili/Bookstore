c8570b1e79c0c8ddab9ca6fa3e1d040bb1b19b64
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-23 00:32:53 +0300

    Update BooksPage.js

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import BookItem from './BookItem'; // Import your BookItem component
-import AddBookForm from './AddBookForm'; // Import the AddBookForm component
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks, addBook, removeBook } from '../redux/books/booksSlice'; // Import the actions
+import BookItem from './BookItem';
+import AddBookForm from './AddBookForm'; 
 
 function BooksPage() {
-  const books = useSelector(state => state.books);
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks()); // Fetch books on component mount
+  }, [dispatch]);
+
+  const handleAddBook = () => {
+    const newBook = {
+      // ... create a new book object ...
+    };
+    dispatch(addBook(newBook)); // Dispatch the addBook action
+  };
+
+  const handleRemoveBook = (bookId) => {
+    dispatch(removeBook(bookId)); // Dispatch the removeBook action
+  };
 
   return (
     <div>
-      {books.map(book => (
-        <div key={book.item_id}>
-          <BookItem book={book} />
-        </div>
-      ))}
-
-      <AddBookForm /> {/* Render the AddBookForm component */}
+      <ul>
+        {books.map((book) => (
+          <BookItem key={book.item_id} book={book} onDeleteBook={() => handleRemoveBook(book.item_id)} />
+        ))}
+      </ul>
+      <AddBookForm/ >
+      {/* ... your add book form ... */}
     </div>
   );
 }
