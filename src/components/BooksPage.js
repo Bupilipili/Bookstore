99a64abbb2e5ece659f2d6f050a9b13b61f4828d
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-25 00:38:19 +0300

    Update BooksPage.js

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBooksAsync } from '../redux/books/booksSlice'; // Adjust import path as needed
+import LoadingSpinner from './LoadingSpinner';
 import BookItem from './BookItem'; // Import the BookItem component
-import AddBookForm from './AddBookForm'; 
+import AddBookForm from './AddBookForm';
+import './BooksPage.css';
 
 function BooksPage() {
   const dispatch = useDispatch();
   const app_id = 'ktMaPkuUjcdRtsd1h31t'; // actual app_id
   const books = useSelector(state => state.books.books);
-  const status = useSelector(state => state.books.status);
+  const isLoading = useSelector(state => state.books.isLoading);
 
   useEffect(() => {
     dispatch(fetchBooksAsync(app_id)); // Fetch books from API using the actual app_id
@@ -16,18 +18,22 @@ function BooksPage() {
 
   return (
     <div>
-     <p>{ status }</p>
-      {
-        books.map((book, index) => (
-          <div key={index}>
-            <BookItem book={book} />
-          </div>
-        ))
-      }
-      <AddBookForm />
+      {isLoading ? (
+        <div className="loading-spinner-container">
+          <LoadingSpinner /> {/* Display the centered loading spinner */}
+        </div>
+      ) : (
+        <div>
+          {books.map((book, index) => (
+            <div key={index}>
+              <BookItem book={book} />
+            </div>
+          ))}
+          <AddBookForm />
+        </div>
+      )}
     </div>
   );
-  
 }
 
 export default BooksPage;
